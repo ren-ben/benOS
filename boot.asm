73d8b8e12880c7ca5e4b9696fe6d00bee3034b35
ren-ben <oprzewlocki@student.tgm.ac.at>
2023-12-23 08:50:31 +0100

    basic hello world bootloader

---
+++ boot.asm
@@ -0,0 +1,27 @@
+ORG 0x7c00
+
+BITS 16
+
+start:
+    mov si, message
+    call print
+    jmp $
+
+print:
+    mov bx, 0
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    call printchar
+    jmp .loop
+.done:
+    ret
+
+printchar:
+    mov ah, 0eh
+    int 0x10
+    ret
+message db 'Hello, World!', 0
+times 510-($-$$) db 0
+dw 0xAA55
