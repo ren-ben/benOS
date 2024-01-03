dccdaac633a07248ee807aae8312c536e94a2dc8
ren-ben <oprzewlocki@student.tgm.ac.at>
2024-01-03 18:42:25 +0100

    implement helpful fat16 structures

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -1,6 +1,113 @@
 #include "fat16.h"
 #include "../../string/string.h"
 #include "../../status.h"
+#include "disk/disk.h"
+#include "disk/streamer.h"
+#include <stdint.h>
+
+#define BENOS_FAT16_SIGNATURE 0x29
+#define BENOS_FAT16_FAT_ENTRY_SIZE 0x02
+#define BENOS_FAT16_BAD_SECTOR 0xFF7
+#define BENOS_FAT16_UNUSED 0x00
+
+typedef unsigned int FAT_ITEM_TYPE;
+#define FAT_ITEM_TYPE_DIR 0
+#define FAT_ITEM_TYPE_FILE 1
+
+// fat dir entry attributes bitmask
+#define FAT_FILE_READ_ONLY 0x01
+#define FAT_FILE_HIDDEN 0x02
+#define FAT_FILE_SYSTEM 0x04
+#define FAT_FILE_VOLUME_LABEL 0x08
+#define FAT_FILE_SUBDIRECTORY 0x10
+#define FAT_FILE_ARCHIVED 0x20
+#define FAT_FILE_DEVICE 0x40
+#define FAT_FILE_RESERVED 0x80
+
+struct fat_header_extended {
+    uint8_t drive_number;
+    uint8_t win_nt_bit;
+    uint8_t signature;
+    uint32_t volume_id;
+    char volume_id_string[11];
+    char system_id_string[8];
+} __attribute__((packed));
+
+struct fat_header {
+    uint8_t short_jump_ins[3];
+    uint8_t oem_identifier[8];
+    uint16_t bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    uint16_t reserved_sectors;
+    uint8_t fat_copies;
+    uint16_t root_dir_entries;
+    uint16_t num_of_sectors;
+    uint8_t media_type;
+    uint16_t sectors_per_fat;
+    uint16_t sectors_per_track;
+    uint16_t num_of_heads;
+    uint32_t hidden_sectors;
+    uint32_t sectors_big;
+} __attribute__((packed));
+
+struct fat_h {
+    struct fat_header primary_header;
+    union hat_h_e {
+        struct fat_header_extended extended_header;
+    } shared;
+};
+
+// fat dir entry
+struct fat_dir_item {
+    uint8_t fname[8];
+    uint8_t ext[3];
+    uint8_t attributes;
+    uint8_t reserved;
+    uint8_t creation_time_ms;
+    uint16_t creation_time;
+    uint16_t creation_date;
+    uint16_t last_access_date;
+    uint16_t high_16_bits_first_cluster;
+    uint16_t last_modification_time;
+    uint16_t last_modification_date;
+    uint16_t low_16_bits_first_cluster;
+    uint32_t file_size;
+} __attribute__((packed));
+
+struct fat_dir {
+    struct fat_dir_item* items;
+    int total;
+    int sector_pos;
+    int ending_sector_pos;
+};
+
+struct fat_item {
+    union {
+        struct fat_dir_item* item;
+        struct fat_dir* dir;
+    };
+
+    FAT_ITEM_TYPE type;
+};
+
+struct fat_item_descriptor {
+    struct fat_item* item;
+    uint32_t pos;
+};
+
+struct fat_private {
+    struct fat_h header;
+    struct fat_dir root_dir;
+
+    // used to stram data clusters
+    struct disk_stream* cluster_read_stream;
+
+    //used to stream the file allocation table
+    struct disk_stream* fat_read_stream;
+
+    // used in situations where we stream the dir
+    struct disk_stream* dir_stream;
+};
 
 int fat16_resolve(struct disk* disk);
 void* fat16_open(struct disk* disk, struct path_part* path, FILE_MODE mode);
