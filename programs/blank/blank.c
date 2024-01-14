9744437ef592a9854adde50140c2da6cd5205c4d
ren-ben <oprzewlocki@student.tgm.ac.at>
2024-01-14 13:48:22 +0100

    add basic stdlib print function

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,3 +1,9 @@
+#include "benos.h"
+
 int main(int argc, char** argv) {
+    
+    print("Hello how are you?\n");
+
+    for (;;) {}
     return 0;
 }
---
+++ programs/stdlib/src/benos.h
@@ -0,0 +1,6 @@
+#ifndef BENOS_H
+#define BENOS_H
+
+void print(const char* fname);
+
+#endif
